b7840623e4e259e9bdeb871d03d691c8df119fd3
Max <m.ch.1.3@mail.ru>
2023-11-29 20:47:57 +0300

    Нарисовал комнаты, запретил Олегу летать над пропастью, уронил Олега

--- Incredible_Adventure_of_Warrior_Oleg.py
+++ Incredible_Adventure_of_Warrior_Oleg.py
@@ -5,9 +5,8 @@ from random import *
 
 adventure_window = Tk()
 adventure_window.title('Невероятное приключение воина по имени Олег')
-adventure_window.maxsize(840, 940)
-adventure_window.minsize(840, 940)
-adventure_window.geometry("+500+20")
+adventure_window.geometry("840x940+500+20")
+adventure_window.resizable(False, False)
 
 canvas_room = Canvas(adventure_window, width=840, height=840, bg='black')
 canvas_room.pack()
@@ -23,19 +22,24 @@ picture_warrior = PhotoImage(file='pic/picture_warrior.png')
 picture_warrior_up = PhotoImage(file='pic/picture_warrior_up.png')
 picture_warrior_left = PhotoImage(file='pic/picture_warrior_left.png')
 picture_warrior_fall = PhotoImage(file='pic/picture_warrior_fall.png')
-picture_warrior_attack = PhotoImage(file='pic/picture_warrior_attack.png')
 picture_pointer = PhotoImage(file='pic/picture_pointer.png')
+picture_warrior_attack = PhotoImage(file='pic/picture_warrior_attack.png')
+picture_warrior_protection = PhotoImage(file='pic/picture_warrior_protection.png')
+
+health_points = 10
+damage = 1
+coins = 0
 
 canvas_room.create_image(420, 420, image = picture_lawn)
 canvas_room.create_image(420, 420, image = picture_trader)
 canvas_room.create_image(420, 420, image = picture_pointer)
 canvas_room.create_image(420, 420, image = picture_warrior)
-
+canvas_text.create_text(420, 50, text = 'Твое здоровье: {0}          Твой урон: {1}          Кол-во монет: {2}'.format(health_points, damage, coins))
 
 def trading(event):
-    trading_window = Toplevel(adventure_window)
-    trading_window.title('tr')
-    # Прописать окно торговли
+        trading_window = Toplevel(adventure_window)
+        trading_window.title('tr')
+        # Прописать окно торговли
 
 
 def leave(event):
@@ -54,6 +58,18 @@ def leave(event):
         sleep(00.01)
     room_geniration()
 
+def meet_monster():
+    meet = randint(1, 500)
+    if meet == 1:
+        monster_window = Toplevel(adventure_window)
+        monster_window.geometry("400x200+500+20")
+        monster_window.resizable(False, False)
+        cfnvas_battle = Canvas(monster_window, width=400, height=200, bg='orange')
+        cfnvas_battle.pack()
+        cfnvas_battle.create_image(10, 49, image=picture_room3)
+        cfnvas_battle.create_image(10,  49, anchor = NW, image=picture_warrior)
+    # else:
+    #     variable -= 1
 
 def leave_room(event):
     room_geniration()
@@ -67,6 +83,7 @@ def open_chest_window(event):
 
 def room_geniration():
     room = randint(1, 3)
+    variable = 1000
     def move(event):
         pos = canvas_room.coords(4)
         if (room == 2 and (pos[0] >= 140 and pos[0] <= 220 and pos[1] >= 40 and pos[1] <= 680 or
@@ -94,6 +111,7 @@ def room_geniration():
             elif event.keysym == 'Right':
                 canvas_room.itemconfigure(4, image = picture_warrior)
                 canvas_room.move(4, 5, 0)
+            meet_monster()
         elif room == 1:
             pass
         else:
