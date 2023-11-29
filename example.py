4b750160d8beaba53aa1858c086e5a7368f5fef0
Max <m.ch.1.3@mail.ru>
2023-11-29 20:48:45 +0300

    Добавил пример

---
+++ example.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+adventure_window = Tk()
+hp = 10
+
+def meet_monster():
+    meet = randint(1, variable)
+    if meet == 1:
+        while 1:
+            hp -= 1
+    else:
+        variable -= 1
+
+variable = 1000
+while 1:
+    meet_monster()
