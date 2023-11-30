923d3dbbba0a8eb8e4d23d9bc87b62195eeb586a
Max <m.ch.1.3@mail.ru>
2023-11-30 06:19:17 +0300

    Изменил пример

--- example.py
+++ example.py
@@ -7,11 +7,10 @@ hp = 10
 def meet_monster():
     meet = randint(1, variable)
     if meet == 1:
-        while 1:
-            hp -= 1
+        hp -= 1
     else:
         variable -= 1
 
 variable = 1000
-while 1:
+while True:
     meet_monster()
